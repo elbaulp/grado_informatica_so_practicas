2cfe6857db74ca701359272c786b1f86df3367e2
Alejandro Alcalde <algui91@gmail.com>
2013-11-24 12:24:24 +0100

    Sesión2: buscar.c terminada

--- Sesion2/src/buscar.c
+++ Sesion2/src/buscar.c
@@ -9,6 +9,45 @@
 
 #include "utils.h"
 
+void explorar_directorio(char *path, char *path_relativo, size_t *suma_total, int *contador_archivos){
+
+    DIR *directorio = opendir(path);
+    if (directorio == NULL) 
+        fatal("Abriendo el directorio");
+    struct dirent *contenido = readdir(directorio); 
+    if (contenido == NULL)
+        fatal("Leyendo el directorio");
+
+    struct stat atributos;
+    
+    while (contenido){
+     
+        while (strcmp(contenido->d_name, ".") == 0 || strcmp(contenido->d_name, "..") == 0)
+            contenido = readdir(directorio);
+
+        strcpy(path_relativo, path);
+        strcat(path_relativo, contenido->d_name);
+        
+        if (stat(path_relativo, &atributos) < 0)
+            fatal("Obteniendo atributos del archivo");
+        if (S_ISREG(atributos.st_mode) 
+                && (atributos.st_mode & S_IRWXG & S_IXGRP) 
+                && (atributos.st_mode & S_IRWXO & S_IXOTH)){
+            printf("%s » %d\n", path_relativo, contenido->d_ino);
+            *(contador_archivos) +=1;
+            *suma_total += atributos.st_size;
+        } else if (S_ISDIR(atributos.st_mode)){
+            strcat(path_relativo, "/");
+            char aux[strlen(path)];
+            /* Para guardar el valor anterior del path al volver de la llamada recursiva */
+            strcpy(aux, path);
+            explorar_directorio(path_relativo, path, suma_total, contador_archivos);
+            strcpy(path, aux);
+        }
+        contenido = readdir(directorio);
+    }
+}
+    
 int main(int argc, char *argv[])
 {
     char *path;
@@ -22,37 +61,17 @@ int main(int argc, char *argv[])
         strcpy(path, argv[1]);
     }
     
-    DIR *directorio = opendir(path);
-    if (directorio == NULL) 
-        fatal("Abriendo el directorio");
-    struct dirent *contenido = readdir(directorio); 
-    if (contenido == NULL)
-        fatal("Leyendo el directorio");
-    struct stat atributos;
-
-    /* Ignoramos . y .. */
-    while(contenido->d_type == 0x4) 
-        contenido = readdir(directorio);
     
-    char* path_relativo = (char*) ec_malloc(strlen(path) + 256); 
+    char *path_relativo = (char*) ec_malloc(strlen(path) + 256); 
     //256 es la longitud maxima del nombre de un fichero, asi aseguramos que no hay buffer overflow
-
+    
     size_t suma_total = 0;
     int contador_archivos = 0;
     
-    while (contenido){
-        strcpy(path_relativo, path);
-        strcat(path_relativo, contenido->d_name);
-        
-        if (stat(path_relativo, &atributos) < 0)
-            fatal("Obteniendo atributos del archivo");
-        if (S_ISREG(atributos.st_mode) 
-                && (atributos.st_mode & S_IRWXG & S_IXGRP) 
-                && (atributos.st_mode & S_IRWXO & S_IXOTH)){
-            printf("%s » %d\n", contenido->d_name, contenido->d_ino);
-        }
-        contenido = readdir(directorio);
-    }
+    printf("Los i-nodos son\n");
+    explorar_directorio(path, path_relativo, &suma_total, &contador_archivos);
+    printf("Existen %d archivos regulares con permiso x para grupo y otros\
+ El tamaño total ocupado por dichos archivos es %d bytes.\n", contador_archivos, suma_total);
 
     free(path);
     free(path_relativo);
